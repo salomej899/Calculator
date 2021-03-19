05376983f7b8c44f7511614cdd25af5533d8e0d0
Salome <salome.javashvili.3@iliauni.edu.ge>
2021-03-19 17:36:39 +0400

    with exception

--- mean.py
+++ mean.py
@@ -1,10 +1,11 @@
-#function that finds the mean of the numbers
+# function that finds the mean of the numbers
 
 def mean(*args):
-    sum1 = sum(args)
+    try:
+        sum1 = sum(args)
+    except TypeError:
+        return "Argument must be an integer or a float"
+
     n = len(args)
     result = sum1/n
     return result
-
-
-
